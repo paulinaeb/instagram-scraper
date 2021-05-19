06b8814f270f69d1920fd01e2e7a051b91da8659
Jose Ameneiro <jameneiroc@gmail.com>
2021-05-19 19:31:08 -0400

    Configure broker url for rabbitmq container on same network

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-MONGO_URI = 'mongodb+srv://admin:admin@clusterbot.t2j5y.mongodb.net/instagramDB?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://admin:admin@clusterbot.t2j5y.mongodb.net/instagramDB?retryWrites=true&w=majority&tlsInsecure=true'
 
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost'
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq-server:5672/'
